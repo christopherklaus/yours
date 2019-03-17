fdc2b96344661e8102c78e82c9626416ef2869c9
Christopher Klaus <christopher@xapix.io>
2019-03-17 18:23:52 +0100

    Initial Commit

---
+++ yours/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  yours
+//
+//  Created by Christopher Klaus on 17.03.19.
+//  Copyright © 2019 Christopher Klaus. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ yours/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  yours
+//
+//  Created by Christopher Klaus on 17.03.19.
+//  Copyright © 2019 Christopher Klaus. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
